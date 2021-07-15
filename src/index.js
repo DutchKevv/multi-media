fc1c2dbf5a48a1f3f76468f91476736b0efeae3b
Kewin Brandsma <kewin@live.nl>
2021-07-15 07:08:08 +0200

    w

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-console.log('post')
+const SocialPost = require("social-post-api");
+const API_KEY = "X6N4E8R-1P94BNN-PFE1WV9-FTMJ301"; // get an API Key at ayrshare.com
+const social = new SocialPost(API_KEY);
 
+// fuck m
+social.post({
+    post: "One more time", // content
+    platforms: ["twitter", "facebook", "linkedin"]
+}).catch(error => {
+    // send SMS / EMAIL to Wytze, Kewin, Indy
+})
